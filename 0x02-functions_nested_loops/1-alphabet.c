681c41cce25988f6133a1b7232a6020227501403
Mildeee <tetteh.mildred222@gmail.com>
2023-04-25 05:42:32 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar (c);
+		c++
+	}
+	_putchar('\n');
+	return (0);
+}
