368f2fafa9073df283ef4432838f61d713fc0046
Mildeee <tetteh.mildred222@gmail.com>
2023-04-27 11:22:01 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - entry point
+ * Description - 'printing 0 to 9 with the exception of 2 and 4'
+ * Return: Always 0 (Successful)
+ */
+
+void print_most_numbers(void)
+{
+	int y = 0;
+
+	for (y = 0; y <= 9; y++)
+	{
+		if (y == 2 || y == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(y + '0');
+		}
+	}
+	_putchar('\n');
+}
